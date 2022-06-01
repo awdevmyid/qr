4b11ff59d1b9ec395a4109b7e5b63e968e0d1705
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-06-01 21:55:19 +0700

    Create redirects.js

---
+++ v1/redirects.js
@@ -0,0 +1,4 @@
+setTimeout(function () {
+   window.location.href = "https://wahyu9kdl.github.io/HTML/TOOLS/QR.html"; //will redirect to your blog page (an ex: blog.html)
+}, 5000); //will call the function after 5 secs.
+Share
