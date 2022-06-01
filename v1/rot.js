b09de24f40ca2f500bcd796118d2479ea12a68a2
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-06-01 21:46:18 +0700

    Create rot.js

---
+++ v1/rot.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Button, Linking } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+       <Button title="Klik To Oppen ROT 13" onPress={ ()=>{ Linking.openURL('https://wahyu9kdl.github.io/DASHBOARD/TOOLS/ROT-13.html')}} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+});
