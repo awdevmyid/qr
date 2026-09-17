1ae7e5613700fb0f8d9363b04dafc3e6f0c71837
awdevmyid <hasansaputramaulana@gmail.com>
2026-09-17 21:21:20 +0700

    Create QrCodeModel.swift

---
+++ AwdevIOS/Models/QrCodeModel.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct QrCodeModel {
+    var type: String = "url"
+    var content: String = "https://awdev.my.id"
+    var backgroundColor: String = "#FFFFFF"
+    var foregroundColor: String = "#1A73E8"
+    var additionalText: String = ""
+}
